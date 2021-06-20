ad686badfc452cce2b7a451a7d17192a9e62f9a0
Finwickle <finwickle@xs4all.nl>
2021-06-20 22:18:24 +0200

    0.3.0.119 ManualUpdater 'action' switch

--- ModChecker/Updater/ManualUpdater.cs
+++ ModChecker/Updater/ManualUpdater.cs
@@ -14,13 +14,13 @@ using ModChecker.Util;
 // * Compatibility: nothing yet
 // * Catalog info:  nothing yet 
 //
-// CSV support is limited (comma separator only, no escaping of comma with quotes) and is in the following format:
-//    Category, Command, Exclusion (true/false), ID, Other command specific data
-//    example: SourceURL, Change, true, 2414618415, https://github.com/kianzarrin/AdvancedRoads
+// CSV lines should be in the following format: Action, ID, Optional other action specific data [Todo 0.3] rewrite
+//    Example: Add_SourceURL, 2414618415, https://github.com/kianzarrin/AdvancedRoads
 //
-// Available categories are listed in the switch statement in ProcessCSV() below.
-// Command can be Add, Remove or Change. Not all categories support all commands.
-// Lines that start with a '#' will be ignored.
+// Available actions are listed in the switch statement in ProcessLine() below.
+// Lines that start with a '#' are considered comments and will be ignored.
+// An exclusion will be automatically created for:
+// * source url, required dlc, required mod, compatible game version (only if the workshop has a game version tag), unlisted status of a mod (to prevent 'removed' status)
 
 
 namespace ModChecker.Updater
@@ -28,7 +28,7 @@ namespace ModChecker.Updater
     internal static class ManualUpdater
     {
         // Stringbuilder to gather the combined CSVs, to be saved with the new catalog
-        internal static StringBuilder CSVComplete;
+        private static StringBuilder CSVCombined;
 
 
         // Start the manual updater; will load and process CSV files, update the active catalog and save it with a new version; including change notes
@@ -39,7 +39,7 @@ namespace ModChecker.Updater
             {
                 return;
             }
-            
+
             Logger.Log("Manual Updater started. See separate logfile for details.");
             Logger.UpdaterLog("Manual Updater started.");
 
@@ -55,9 +55,9 @@ namespace ModChecker.Updater
                 string partialPath = CatalogUpdater.Start("the Manual Updater process");
 
                 // Save the combined CSVs to one file, next to the catalog
-                if (!string.IsNullOrEmpty(partialPath) && CSVComplete.Length != 0)
+                if (!string.IsNullOrEmpty(partialPath) && CSVCombined.Length != 0)
                 {
-                    Toolkit.SaveToFile(CSVComplete.ToString(), partialPath + "_ManualUpdates.csv");
+                    Toolkit.SaveToFile(CSVCombined.ToString(), partialPath + "_ManualUpdates.csv");
                 }
 
                 if (success == false)
@@ -67,7 +67,7 @@ namespace ModChecker.Updater
             }
 
             // Clean up
-            CSVComplete = null;
+            CSVCombined = null;
 
             Logger.UpdaterLog("Manual Updater has shutdown.", extraLine: true, duplicateToRegularLog: true);
         }
@@ -98,43 +98,43 @@ namespace ModChecker.Updater
             {
                 Logger.UpdaterLog($"Processing \"{ CSVfile }\".");
 
+                bool singleFileSuccess = true;
+
                 // Read a single CSV file
                 using (StreamReader reader = File.OpenText(CSVfile))
                 {
-                    bool singleFileSuccess = true;
-
                     // Read each line and process the command
                     while ((line = reader.ReadLine()) != null)
                     {
                         // Process a line
-                        if (ProcessCommand(line))
+                        if (ProcessLine(line))
                         {
                             // Add this line to the complete list
-                            CSVComplete.AppendLine(line);   // [Todo 0.3] append or appendline?
+                            CSVCombined.AppendLine(line);   // [Todo 0.3] append or appendline?
                         }
                         else
                         {
                             // Add the failed line with a comment to the complete list
-                            CSVComplete.AppendLine("# [NOT PROCESSED]: " + line);   // [Todo 0.3] append or appendline?
+                            CSVCombined.AppendLine("# [NOT PROCESSED]: " + line);   // [Todo 0.3] append or appendline?
 
                             singleFileSuccess = false;
 
                             overallSuccess = false;
                         }
                     }
+                }
 
-                    // Rename the processed CSV file
-                    string newFileName = CSVfile + (singleFileSuccess ? "_processed.txt" : "_processes_partially.txt");
+                // Rename the processed CSV file
+                string newFileName = CSVfile + (singleFileSuccess ? "_processed.txt" : "_processes_partially.txt");
 
-                    if (!Toolkit.MoveFile(CSVfile, newFileName))
-                    {
-                        Logger.UpdaterLog($"Could not rename \"{ CSVfile }\". Rename or delete it manually to avoid processing it again.", Logger.error);
-                    }
+                if (!Toolkit.MoveFile(CSVfile, newFileName))
+                {
+                    Logger.UpdaterLog($"Could not rename \"{ CSVfile }\". Rename or delete it manually to avoid processing it again.", Logger.error);
+                }
 
-                    if (!singleFileSuccess)
-                    {
-                        Logger.UpdaterLog($"\"{ newFileName }\" not processed (completely).", Logger.warning);
-                    }
+                if (!singleFileSuccess)
+                {
+                    Logger.UpdaterLog($"\"{ newFileName }\" not processed (completely).", Logger.warning);
                 }
             }
 
@@ -142,64 +142,339 @@ namespace ModChecker.Updater
         }
 
 
-        private static bool ProcessCommand(string line)
+        // Process a line from a CSV file
+        private static bool ProcessLine(string line)
         {
-            // Split the line
+            // Skip comments starting with a '#'
+            if (line[0] == '#')
+            {
+                return true;
+            }
+
+            // Split the line; exit if we don't have enough elements
             string[] lineFragments = line.Split(',');
 
-            string category = lineFragments[0].Trim().ToLower();
-
-            switch (category)
+            if (lineFragments.Length < 2)
             {
-                case "mod": 
-                    // ...
+                return NotEnoughElements(line);
+            }
+
+            // Get the action
+            string action = lineFragments[0].Trim().ToLower();
+
+            // Get the id as string (for author custom url) and number (for Steam IDs); 0 if unrecognized
+            string idString = lineFragments[1].Trim();
+            ulong id;
+
+            try
+            {
+                id = Convert.ToUInt64(idString);
+            }
+            catch
+            {
+                id = 0;
+            }
+
+            // Get the rest of the data, if present
+            ulong secondID = 0;
+            string extraData = "";
+            
+            if (lineFragments.Length > 2)
+            {
+                extraData = lineFragments[2];
+
+                try
+                {
+                    secondID = Convert.ToUInt64(extraData);
+                }
+                catch
+                {
+                    secondID = 0;
+                }
+            }
+
+            if (secondID > 0 && lineFragments.Length > 3)
+            {
+                extraData = lineFragments[3];
+            }
+
+            bool success;
+
+            // Act on the action and category found
+            switch (action)
+            {
+                case "add_mod":
+                    success = AddMod(id, extraData == "removed");
                     break;
 
-                case "group":
-                case "modgroup":
-                    // ...
+                case "remove_mod":
+                    success = RemoveMod(id);
                     break;
 
-                case "compatibility":
-                case "modcompatibility":
-                    // ...
+                case "add_archiveurl":
+                case "add_sourceurl":
+                case "add_gameversion":
+                case "add_requireddlc":
+                case "add_status":
+                    success = AddModItem(id, action, extraData);
                     break;
 
-                case "archiveurl":
-                case "sourceurl": 
-                    // ...
+                case "add_note":
+                    success = AddModItem(id, action, string.Join(", ", lineFragments, 2, lineFragments.Length - 2).Trim());
                     break;
 
-                default: 
-                    // ...
+                case "add_requiredmod":
+                case "add_neededfor":
+                case "add_successor":
+                case "add_alternative":
+                    success = AddModItem(id, action, listMember: secondID);
+                    break;
+
+                case "remove_archiveurl":
+                case "remove_sourceurl":
+                case "remove_gameversion":
+                case "remove_requireddlc":
+                case "remove_status":
+                case "remove_note":
+                    success = RemoveModItem(id, action);
+                    break;
+
+                case "remove_requiredmod":
+                case "remove_neededfor":
+                case "remove_successor":
+                case "remove_alternative":
+                    success = RemoveModItem(id, action, listMember: secondID);
+                    break;
+
+                case "add_authorid":
+                    success = AddAuthorItem(authorURL: idString, newAuthorID: secondID);
+                    break;
+
+                case "add_authorurl":
+                case "add_lastseen":
+                case "add_retired":
+                    if (id == 0)
+                    {
+                        success = AddAuthorItem(authorURL: idString, action, extraData);
+                    }
+                    else
+                    {
+                        success = AddAuthorItem(authorID: id, action, extraData);
+                    }                    
+                    break;
+
+                case "remove_authorurl":
+                case "remove_lastseen":
+                case "remove_retired":
+                    if (id == 0)
+                    {
+                        success = RemoveAuthorItem(authorURL: idString, action);
+                    }
+                    else
+                    {
+                        success = RemoveAuthorItem(authorID: id, action);
+                    }
+                    break;
+
+                case "add_group":
+                case "add_modgroup":
+                    if (lineFragments.Length < 4)
+                    {
+                        success = NotEnoughElements(line);
+                    }
+                    else
+                    {
+                        List<string> groupMembers = lineFragments.ToList();
+
+                        groupMembers.RemoveRange(0, 2);
+
+                        success = AddGroup(name: idString, groupMembers);
+                    }
+                    break;
+
+                case "remove_group":
+                case "remove_modgroup":
+                    success = RemoveGroup(id, replacementMod: secondID);
+                    break;
+
+                case "add_groupmember":
+                    success = AddGroupMember(id, groupMember: secondID);
+                    break;
+
+                case "remove_groupmember":
+                    success = RemoveGroupMember(id, groupMember: secondID);
+                    break;
+
+                case "add_compatibility":
+                    success = AddCompatibility(steamID1: id, steamID2: secondID, lineFragments); // [Todo 0.3] Can do better?
+                    break;
+
+                case "remove_compatibility":
+                    success = RemoveCompatibility(steamID1: id, steamID2: secondID);
+                    break;
+
+                case "remove_exclusion":
+                    success = RemoveExclusion(name: idString, category: extraData, subitem: secondID);
+                    break;
+
+                case "add_catalognote":
+                case "remove_catalognote":
+                    CatalogUpdater.SetNote(action[0] == 'a' ? string.Join(", ", lineFragments, 2, lineFragments.Length - 2).Trim() : "");
+                    success = true;
+                    break;
+
+                case "add_cataloggameversion":
+                    success = SetCatalogGameVersion(gameVersionString: idString);
+                    break;
+
+                default:
+                    Logger.UpdaterLog($"Line not processed, invalid action  \"{ action }\". Line: { line }", Logger.warning);
+                    success = false;
                     break;
             }
-            /* 
-            Mod,
-            ArchiveURL,
-            SourceURL,
-            CompatibleGameVersion,
-            RequiredMod,
-            RequiredDLC,
-            NeededFor,
-            Succeessor,
-            Alternative,
-            Status,
-            Note,
-            Group,
-            GroupMember,
-            Compatibility,
-            AuthorID,
-            AuthorURL,
-            LastSeen,
-            Retired,
-            Exclusion,
-            CatalogNote,
-            CatalogGameVersion,
-            IntroText,
-            FooterText
-            */
 
+            return success;
+        }
+
+
+        // Log an error for having too few elements in the CSV line
+        private static bool NotEnoughElements(string line)
+        {
+            Logger.UpdaterLog($"Line not processed, not enough elements. Line: { line }", Logger.warning);
+
+            return false;
+        }
+
+
+        // 
+        private static bool AddMod(ulong steamID, bool removedFromWorkshop)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveMod(ulong steamID)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddModItem(ulong steamID, string action, ulong listMember)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddModItem(ulong steamID, string action, string itemData)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveModItem(ulong steamID, string action)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveModItem(ulong steamID, string action, ulong listMember)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddAuthorItem(ulong authorID, string action, string itemData)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddAuthorItem(string authorURL, string action, string itemData)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddAuthorItem(string authorURL, ulong newAuthorID)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveAuthorItem(string authorURL, string action)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveAuthorItem(ulong authorID, string action)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddGroup(string name, List<string> groupMembers)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveGroup(ulong groupID, ulong replacementMod)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddGroupMember(ulong groupID, ulong groupMember)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveGroupMember(ulong groupID, ulong groupMember)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool AddCompatibility(ulong steamID1, ulong steamID2, string[] compatibilityData)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveCompatibility(ulong steamID1, ulong steamID2)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool RemoveExclusion(string name, string category, ulong subitem)
+        {
+            return false;
+        }
+
+
+        // 
+        private static bool SetCatalogGameVersion(string gameVersionString)
+        {
             return false;
         }
     }
