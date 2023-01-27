cebd870324600afaa1f3efa070e4429a805b6cf3
Chamëleon TBN <37211016+chameleon-tbn@users.noreply.github.com>
2023-01-27 10:58:39 +0100

    Update ModSettings.cs

--- CompatibilityReport/Util/ModSettings.cs
+++ CompatibilityReport/Util/ModSettings.cs
@@ -9,12 +9,12 @@ namespace CompatibilityReport.Util
     public static class ModSettings
     {
         // Mod properties.
-        public const string Version = "2.3.1";
+        public const string Version = "2.3.2";
 #if DEBUG
         // allow for hot-swapping the mod - rebuild only if it's in the main menu, game will detect and reload the mod
         public const string Build = "*";
 #else
-        public const string Build = "444";
+        public const string Build = "445";
 #endif
         public const string ReleaseType = "";
         public const int CurrentCatalogStructureVersion = 7;
